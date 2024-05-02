fe635daa5bb77b3c67e59306f7df9cfb5d6cf673
Elmaz <kakbyelmaz@gmail.com>
2024-05-02 14:58:31 +0200

    add

--- src/main/java/intecbrussel/be/Main.java
+++ src/main/java/intecbrussel/be/Main.java
@@ -4,6 +4,21 @@ import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 import intecbrussel.be.model.MedicalFile;
 import intecbrussel.be.model.Patient;
 
+
+
+/*One-To-One
+Je maakt een Object Patient. Geef die een String rijksregisternummer, een String naam, boolean sick.
+Dan, een MedicalFile object. Met een int id, een Patient variabele en een String illnes.
+We gaan een paar patienten opmaken, en die elk een Medical File geven. Wat is logisch? Wie is de
+owner?*/
+
+/*1.) One-To-One
+You create an Object Patient. Give it a String national register number, a String name, boolean sick.
+Then, a MedicalFile object. With an int id, a Patient variable and a String illnes.
+We are going to prepare a few patients and give each of them a Medical File. What makes sense? Who is the owner?*/
+
+
+
 public class Main {
     public static void main(String[] args) {
         // Массив с именами пациентов
