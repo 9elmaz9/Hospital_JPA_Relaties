a5a9d1ce39678c1298189bb5c09cae77affc69e0
Elmaz <kakbyelmaz@gmail.com>
2024-05-02 15:49:01 +0200

    add

--- src/main/java/intecbrussel/be/model/MedicalFile.java
+++ src/main/java/intecbrussel/be/model/MedicalFile.java
@@ -3,9 +3,19 @@ package intecbrussel.be.model;
 import jdk.jfr.DataAmount;
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "medicalfile")
 public class MedicalFile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="id")
     private int id;
+
+    @OneToOne
+    @JoinColumn(name = "patient-id")
     private Patient patient;
     private String illness;
 
--- src/main/java/intecbrussel/be/model/Patient.java
+++ src/main/java/intecbrussel/be/model/Patient.java
@@ -1,15 +1,24 @@
 package intecbrussel.be.model;
 
 import lombok.Data;
+
+import javax.persistence.*;
+
 //Patient de 'eigenaar' van de relatie
 //срок жизни объекта MedicalFile зависит от объекта Patient
 //Если пациент удален, соответствующая медицинская запись также должна быть удалена
 @Data
+@Entity
+@Table(name ="patient")
 public class Patient {
-
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "id")
     private String rijksregisternummer;
+
     private String naam;
     private boolean sick;
+    @OneToOne(mappedBy = "patient", cascade =CascadeType.ALL)
     private MedicalFile medicalFile;
 
 }
